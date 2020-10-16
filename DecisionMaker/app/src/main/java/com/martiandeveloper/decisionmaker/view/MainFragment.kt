55c7accef82783c223f11305e1e8f78a7f640ed7
mdismayilov <murad.ismaylov.97@gmail.com>
2020-10-16 14:56:09 +0400

    The main fragment is ready

--- DecisionMaker/app/src/main/java/com/martiandeveloper/decisionmaker/view/MainFragment.kt
+++ DecisionMaker/app/src/main/java/com/martiandeveloper/decisionmaker/view/MainFragment.kt
@@ -2,25 +2,31 @@
 
 package com.martiandeveloper.decisionmaker.view
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import com.martiandeveloper.decisionmaker.R
+import com.martiandeveloper.decisionmaker.databinding.DialogOptionBinding
 import com.martiandeveloper.decisionmaker.databinding.FragmentMainBinding
 import com.martiandeveloper.decisionmaker.viewmodel.MainViewModel
+import timber.log.Timber
+
 
 class MainFragment : Fragment() {
 
     private lateinit var fragmentMainBinding: FragmentMainBinding
 
     private lateinit var mainViewModel: MainViewModel
+
+    private lateinit var optionDialog: AlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +47,10 @@ class MainFragment : Fragment() {
 
         fragmentMainBinding.mainViewModel = mainViewModel
         fragmentMainBinding.lifecycleOwner = this
+
+        observe()
+
+        optionDialog = AlertDialog.Builder(context).create()
     }
 
     private fun getViewModel(): MainViewModel {
@@ -56,6 +66,75 @@ class MainFragment : Fragment() {
 
     private fun navigate(navDirections: NavDirections) {
         view?.let { Navigation.findNavController(it).navigate(navDirections) }
+    }
+
+    private fun observe() {
+
+        mainViewModel.eventOptionMBTNClick.observe(viewLifecycleOwner, {
+            if (it) {
+                if (!optionDialog.isShowing) {
+                    mainViewModel.setIsOptionDialogShowing(true)
+                }
+                mainViewModel.onOptionMBTNClickComplete()
+            }
+        })
+
+        mainViewModel.eventDecideMBTNClick.observe(viewLifecycleOwner, {
+            if (it) {
+                Timber.i("Decide")
+                mainViewModel.onDecideMBTNClickComplete()
+            }
+        })
+
+        mainViewModel.eventCancelMBTNClick.observe(viewLifecycleOwner, {
+            if (it) {
+                if (optionDialog.isShowing) {
+                    mainViewModel.setIsOptionDialogShowing(false)
+                }
+                mainViewModel.onCancelMBTNClickComplete()
+            }
+        })
+
+        mainViewModel.eventDoneMBTNClick.observe(viewLifecycleOwner, {
+            if (it) {
+                if (optionDialog.isShowing) {
+                    mainViewModel.setIsOptionDialogShowing(false)
+                }
+
+                val text = mainViewModel.optionsMTV.value
+
+                if (text != null) {
+                    mainViewModel.setOptionsMTVText("$text\n${mainViewModel.optionET.value}")
+                } else {
+                    mainViewModel.setOptionsMTVText("${mainViewModel.optionET.value}")
+                }
+
+                mainViewModel.onDoneMBTNClickComplete()
+            }
+        })
+
+        mainViewModel.isOptionDialogShowing.observe(viewLifecycleOwner, {
+            if (it) {
+                showOptionDialog()
+            } else {
+                optionDialog.dismiss()
+            }
+        })
+
+    }
+
+    private fun showOptionDialog() {
+        val binding = DialogOptionBinding.inflate(LayoutInflater.from(context))
+
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
+
+        binding.dialogOptionOptionET.requestFocus()
+
+        optionDialog.setView(binding.root)
+        optionDialog.setCanceledOnTouchOutside(false)
+        optionDialog.setCancelable(false)
+        optionDialog.show()
     }
 
 }
--- DecisionMaker/app/src/main/java/com/martiandeveloper/decisionmaker/viewmodel/MainViewModel.kt
+++ DecisionMaker/app/src/main/java/com/martiandeveloper/decisionmaker/viewmodel/MainViewModel.kt
@@ -6,8 +6,78 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    private var _eventContinueMBTNClick = MutableLiveData<Boolean>()
-    val eventContinueMBTNClick: LiveData<Boolean>
-        get() = _eventContinueMBTNClick
+    var questionET = MutableLiveData<String>()
+
+    private var _eventOptionMBTNClick = MutableLiveData<Boolean>()
+    val eventOptionMBTNClick: LiveData<Boolean>
+        get() = _eventOptionMBTNClick
+
+    private var _eventDecideMBTNClick = MutableLiveData<Boolean>()
+    val eventDecideMBTNClick: LiveData<Boolean>
+        get() = _eventDecideMBTNClick
+
+    var optionET = MutableLiveData<String>()
+
+    private var _eventDoneMBTNClick = MutableLiveData<Boolean>()
+    val eventDoneMBTNClick: LiveData<Boolean>
+        get() = _eventDoneMBTNClick
+
+    private var _eventCancelMBTNClick = MutableLiveData<Boolean>()
+    val eventCancelMBTNClick: LiveData<Boolean>
+        get() = _eventCancelMBTNClick
+
+    private var _isOptionDialogShowing = MutableLiveData<Boolean>()
+    val isOptionDialogShowing: LiveData<Boolean>
+        get() = _isOptionDialogShowing
+
+    private var _optionsMTV = MutableLiveData<String>()
+    val optionsMTV: LiveData<String>
+        get() = _optionsMTV
+
+
+    init {
+        _isOptionDialogShowing.value = false
+    }
+
+
+    fun onOptionMBTNClick() {
+        _eventOptionMBTNClick.value = true
+    }
+
+    fun onOptionMBTNClickComplete() {
+        _eventOptionMBTNClick.value = false
+    }
+
+    fun onDecideMBTNClick() {
+        _eventDecideMBTNClick.value = true
+    }
+
+    fun onDecideMBTNClickComplete() {
+        _eventDecideMBTNClick.value = false
+    }
+
+    fun onDoneMBTNClick() {
+        _eventDoneMBTNClick.value = true
+    }
+
+    fun onDoneMBTNClickComplete() {
+        _eventDoneMBTNClick.value = false
+    }
+
+    fun onCancelMBTNClick() {
+        _eventCancelMBTNClick.value = true
+    }
+
+    fun onCancelMBTNClickComplete() {
+        _eventCancelMBTNClick.value = false
+    }
+
+    fun setIsOptionDialogShowing(show: Boolean) {
+        _isOptionDialogShowing.value = show
+    }
+
+    fun setOptionsMTVText(text: String) {
+        _optionsMTV.value = text
+    }
 
 }
